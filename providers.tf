006479dbf9a54da5d7142b9695e4d78783ed312c
RAJESH BADUWAL <61152071+Rajesh2199@users.noreply.github.com>
2022-10-04 00:47:36 -0700

    removed secret key.

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAT6K4GNB2BLHMIZUX"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
